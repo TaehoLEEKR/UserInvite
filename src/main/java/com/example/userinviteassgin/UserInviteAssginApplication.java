59965c1a9554943e125c44809c89919f400ec437
TaehoLEEKR <679748@naver.com>
2023-08-08 19:39:47 +0900

    first commit

---
+++ src/main/java/com/example/userinviteassgin/UserInviteAssginApplication.java
@@ -0,0 +1,13 @@
+package com.example.userinviteassgin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInviteAssginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserInviteAssginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/userinviteassgin/UserInviteAssginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userinviteassgin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInviteAssginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
